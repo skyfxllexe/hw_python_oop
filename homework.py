f7534e8cd3c861a06f5302982d94b1e3b972c9bf
Yan Zerkalin <discxnnectedexehse@gmail.com>
2023-02-14 05:01:18 +0300

    cs

--- homework.py
+++ homework.py
@@ -181,8 +181,7 @@ def read_package(workout_type: str, data: list) -> Training:
 
 def main(training: Training) -> None:
     """Главная функция."""
-    info: InfoMessage = training.show_training_info()
-    print(info.get_message())
+    print(training.show_training_info().get_message())
 
 
 if __name__ == '__main__':
